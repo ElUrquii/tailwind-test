ed6bb111bca4e22441c483c9a6a703797dbb283b
ElUrquii <urquizamaxi099@gmail.com>
2021-08-24 11:36:08 -0300

    Minor changes to text, changed default css styles

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-6xl font-sans font-extrabold text-gray-800 subpixel-antialiased text-center">
+      <h1 className="text-6xl font-extrabold text-gray-800 antialiased text-center">
         Tailwind
       </h1>
     </div>
