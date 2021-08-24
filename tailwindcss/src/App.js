6687ae9909f9c6a9139b508e6c30430289327604
ElUrquii <urquizamaxi099@gmail.com>
2021-08-24 11:08:48 -0300

    Tailwind init, little demo

---
+++ tailwindcss/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl font-sans font-extrabold text-gray-800 subpixel-antialiased text-center">
+        Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
