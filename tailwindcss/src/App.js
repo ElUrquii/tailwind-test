b6be072b6a8bbb23feee964e5fa82c03b7f5fc1b
ElUrquii <urquizamaxi099@gmail.com>
2021-09-13 15:14:36 -0300

    Search Filter added

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -1,30 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./components/UI/Card";
-import axios from 'axios'
+import axios from "axios";
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
+  const [filteredData, setFilteredData] = useState(coins);
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then((response) => {
-      setCoins(response.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }, [])
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        setCoins(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleSearchValue = (event) => {
+    let resultadoBusqueda = event.target.value;
+
+    let result = [];
+    result = coins.filter((coin) => {
+      return coin.id.search(resultadoBusqueda) !== -1;
+    });
+
+    setFilteredData(result);
+  };
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto my-6">
       <h1 className="text-6xl font-extrabold text-gray-800 antialiased text-center mb-6">
         Crypto Viewer
       </h1>
-      <div className='mx-4 grid gap-4 grid-cols-1 md:grid-cols-3 justify-items-stretch'>
-        {coins.map((coin) => {
-          return(
-            <Card img={coin.image} name={coin.name} price={coin.current_price} />
-          )
+      <div className="flex justify-center w-full">
+        <input
+          placeholder="Search Coin"
+          className="mb-8 py-2 px-3 shadow-md rounded-md"
+          onChange={handleSearchValue}
+        />
+      </div>
+      <div className="mx-4 grid gap-4 grid-cols-1 md:grid-cols-3 justify-items-stretch">
+        {filteredData.map((coin) => {
+          return (
+            <Card
+              key={coin.market_cap_rank}
+              img={coin.image}
+              name={coin.symbol}
+              price={coin.current_price}
+              coinId={coin.id}
+            />
+          );
         })}
       </div>
     </div>
--- tailwindcss/src/components/UI/Card.js
+++ tailwindcss/src/components/UI/Card.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 const Card = (props) => {
+  const redirectToCryptoPage = () => {
+    axios
+      .get("https://api.coingecko.com/api/v3/coins/" + props.coinId)
+      .then((response) => {
+        window.location.href = response.data.links.homepage[0];
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-      <div className='flex flex-col py-0 px-0 justify-self-auto border-2 border-gray-200 rounded-md shadow-xl'>
-        <div className='flex flex-row py-0 px-0 mx-2 my-2'>      
-          <img src={props.img} alt='crypto logo' className='pr-2' style={{width: '50px', height: '50px'}} />
-          <h2 className='text-4xl font-extrabold text-gray-800 antialiased text-center my-2'>
-          {props.name}
+      <div className="flex flex-col py-0 px-0 justify-self-auto border-2 border-gray-200 rounded-md shadow-xl">
+        <div className="flex flex-row py-0 px-0 mx-2 my-2">
+          <img
+            src={props.img}
+            alt="crypto logo"
+            className="pr-2"
+            style={{ width: "auto", height: "50px" }}
+          />
+          <h2 className="uppercase text-4xl font-extrabold text-gray-800 antialiased text-center my-2">
+            {props.name}
           </h2>
         </div>
-        <p className='text-base font-light text-gray-800 mx-2 my-2'>Current Price: ${props.price}</p>
-        <div className='grid grid-cols-2 my-2'>
-          <button className='rounded-md font-bold bg-gray-800 text-gray-100 py-2  mx-2 transition duration-250 ease-in-out hover:bg-gray-900'>Comprar</button>
-          <button className='rounded-md font-bold bg-transparent border-2 border-gray-800 text-gray-800  mx-2 py-2 transition duration-250 ease-in-out hover:bg-gray-800 hover:text-gray-100'>Mas Info</button>
+        <p className="capitalize text-base font-light text-gray-800 mx-2 my-2">
+          Name: {props.coinId}
+        </p>
+        <p className="text-base font-light text-gray-800 mx-2 my-2">
+          Current Price: ${props.price}
+        </p>
+        <div className="grid grid-cols-2 my-2">
+          <button className="rounded-md font-bold bg-gray-800 text-gray-100 py-2  mx-2 transition duration-250 ease-in-out hover:bg-gray-900">
+            Comprar
+          </button>
+          <button
+            onClick={redirectToCryptoPage}
+            className="rounded-md font-bold bg-transparent border-2 border-gray-800 text-gray-800  mx-2 py-2 transition duration-250 ease-in-out hover:bg-gray-800 hover:text-gray-100"
+          >
+            Mas Info
+          </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
