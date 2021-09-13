86292dbd0921e7f62beb366ecb253f52650099a7
ElUrquii <urquizamaxi099@gmail.com>
2021-09-13 11:09:34 -0300

    coingecko api call, coins displayed in cards

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Card from "./components/UI/Card";
+import axios from 'axios'
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((response) => {
+      setCoins(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className="container mx-auto">
-      <h1 className="text-6xl font-extrabold text-gray-800 antialiased text-center">
-        Tailwind
+      <h1 className="text-6xl font-extrabold text-gray-800 antialiased text-center mb-6">
+        Crypto Viewer
       </h1>
       <div className='mx-4 grid gap-4 grid-cols-1 md:grid-cols-3 justify-items-stretch'>
-        <Card />
-        <Card />
-        <Card />
+        {coins.map((coin) => {
+          return(
+            <Card img={coin.image} name={coin.name} price={coin.current_price} />
+          )
+        })}
       </div>
     </div>
   );
--- tailwindcss/src/components/UI/Card.js
+++ tailwindcss/src/components/UI/Card.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <>
       <div className='flex flex-col py-0 px-0 justify-self-auto border-2 border-gray-200 rounded-md shadow-xl'>
-        <h2 className='text-4xl font-extrabold text-gray-800 antialiased text-center my-2'>
-          Hola
-        </h2>
-        <p className='text-base font-light text-gray-800 mx-2 my-2'>Lorem la puta de tu mama</p>
+        <div className='flex flex-row py-0 px-0 mx-2 my-2'>      
+          <img src={props.img} alt='crypto logo' className='pr-2' style={{width: '50px', height: '50px'}} />
+          <h2 className='text-4xl font-extrabold text-gray-800 antialiased text-center my-2'>
+          {props.name}
+          </h2>
+        </div>
+        <p className='text-base font-light text-gray-800 mx-2 my-2'>Current Price: ${props.price}</p>
         <div className='grid grid-cols-2 my-2'>
           <button className='rounded-md font-bold bg-gray-800 text-gray-100 py-2  mx-2 transition duration-250 ease-in-out hover:bg-gray-900'>Comprar</button>
           <button className='rounded-md font-bold bg-transparent border-2 border-gray-800 text-gray-800  mx-2 py-2 transition duration-250 ease-in-out hover:bg-gray-800 hover:text-gray-100'>Mas Info</button>
