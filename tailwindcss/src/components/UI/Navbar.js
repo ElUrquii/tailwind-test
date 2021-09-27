f1b21254e9fcc29143fbd103fda11d2e25fbd629
ElUrquii <urquizamaxi099@gmail.com>
2021-09-27 11:29:46 -0300

    Mobile menu navigation done

--- tailwindcss/src/components/UI/Navbar.js
+++ tailwindcss/src/components/UI/Navbar.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaStream } from "react-icons/fa";
+import { MdClear } from "react-icons/md";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <>
       <div className="flex justify-between container mx-auto border-b-2 h-16">
@@ -40,10 +47,53 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex flex-row items-center sm:hidden">
-          <FaStream className="text-2xl mx-2 cursor-pointer" />
+          {!active ? (
+            <FaStream
+              className="text-2xl mx-2 cursor-pointer"
+              onClick={handleClick}
+            />
+          ) : (
+            <MdClear
+              className="text-4xl mx-2 cursor-pointer"
+              onClick={handleClick}
+            />
+          )}
         </div>
       </div>
-      <div className="sm:hidden"></div>
+      {active && (
+        <div className="sm:hidden h-screen bg-gray-800">
+          <div className="h-screen flex flex-col items-center">
+            <Link
+              to="/trending"
+              className="text-xl text-gray-100 transition duration-200 ease-in-out hover:text-blue-400 my-7"
+              onClick={handleClick}
+            >
+              Trending
+            </Link>
+            <Link
+              to="/categories"
+              className="text-xl text-gray-100 transition duration-200 ease-in-out hover:text-blue-400 my-7"
+              onClick={handleClick}
+            >
+              Categories
+            </Link>
+            <Link
+              to="/exchanges"
+              className="text-xl text-gray-100 transition duration-200 ease-in-out hover:text-blue-400 my-7"
+              onClick={handleClick}
+            >
+              Exchanges
+            </Link>
+            <Link
+              to="/news"
+              className="text-xl text-gray-100 transition duration-200 ease-in-out hover:text-blue-400 my-7"
+              onClick={handleClick}
+            >
+              News
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 };
